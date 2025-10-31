43e1b81fa07e298dd4dda61cd614dc4c9a6e1fc7
michaelkinda <153565102+michaelkinda@users.noreply.github.com>
2025-10-30 16:59:01 -0800

    Add transactions controller tests

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -2,12 +2,22 @@ require "test_helper"
 
 class TransactionsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get transactions_index_url
+    get transactions_url
     assert_response :success
   end
 
-  test "should get create" do
-    get transactions_create_url
-    assert_response :success
+  test "should create transaction" do
+    from_account = accounts(:one)
+    to_account = accounts(:two)
+
+    assert_difference "Transaction.count", 1 do
+      post transactions_url, params: {
+        amount: 5.00,
+        from_account_id: from_account.id,
+        to_account_id: to_account.id
+      }
+    end
+
+    assert_redirected_to transactions_path
   end
 end
